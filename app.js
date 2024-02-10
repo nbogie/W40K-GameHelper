e62f2430754309da5c6e509eb4185626838e66e5
d-atienza <153103146+d-atienza@users.noreply.github.com>
2024-02-10 21:16:10 +0000

    added in support folder files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { query } = require("./support/db");
 const { diceRolling, turnCounter } = require("./functions");
 
 const app = express();
@@ -10,66 +11,54 @@ app.set("view engine", "ejs");
 
 ////////////////////////////
 
-//elephant sql db
-const dotenv = require("dotenv");
-dotenv.config(); // looks for env file with url
-const pool = new Pool({ connectionString: process.env.DATABASE_URL }); // keeps your password and url a secret!
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-async function dbDisplay() {
-  const dbResult = await pool.query("select * from queen_songs limit 10");
-  const songArray = dbResult.rows;
-  return songArray;
-}
-
-////////////////////////////
-
-app.get("/", async (req, res) => {
-  const player1 = req.query.player1;
-  const player2 = req.query.player2;
+app.post("/points", async (req, res) => {
+  const player1 = req.body.player1;
+  const player2 = req.body.player2;
 
   const value1 = [`%${player1}%`];
   const value2 = [`%${player2}%`];
 
-  const insertPlayers = await pool.query(
+  await query(
     "insert into points ( player, primaryPoints,secondaryPoints, commandPoints) values (($1)",
     value1,
     "0, 0, 0), (($1)",
     value2,
     "0, 0, 0); "
-  );
+  ); // inserts into points table
 
-  const startTurnCounter = await pool.query(
-    "insert into turn_number (turnNum) values (0);"
-  );
-
-  res.render(
-    "index",
-    { players: insertPlayers },
-    { turnCounter: startTurnCounter }
-  );
-});
-
-/**
-insert into points (
-    player,
-    primaryPoints,
-    secondaryPoints,
-    commandPoints,
-    )
-values 
-    ('neill', 150, now()),
-    ('kenny', 650, now()),
-    ('lauren', 700, now());
- */
-
-app.get("/points", (req, res) => {
-  turnCounter(nextTurn, prevTurn, currTurnNum);
+  await query("insert into turn_number (turnNum) values (0);"); // inserts into turn table
 
   res.render("points");
 });
+
+////////////////////////////
+
+app.get("/points", (req, res) => {
+  turnCounter(nextTurn, prevTurn, currTurnNum); //STILL NEED TO CREATE THIS
+
+  res.render("points");
+});
+
+////////////////////////////
 
 app.get("/diceroll", (req, res) => {
   let inputDice = req.query.diceNumber;
 
   res.render("diceroll", { resultObj: diceRolling(inputDice) });
+});
+
+////////////////////////////
+
+// use the environment variable PORT, or 3000 as a fallback if it is undefined
+const PORT_NUMBER = process.env.PORT ?? 3000;
+
+//start the server listening indefinitely
+app.listen(PORT_NUMBER, () => {
+  console.log(
+    `Your express app started listening on ${PORT_NUMBER} at ${new Date()}`
+  );
 });
